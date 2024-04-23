1384ae568dcfd41074368c5500cf9cf6af70dd80
Yasu3D <l.frisan31@gmail.com>
2024-04-23 20:37:51 +0200

    make bgm progress bar track EndOfChart note instead of audio end.
    Noticed this while playing mercury.

--- Assets/__Scripts/UI/RhythmGame/BgmProgressBar.cs
+++ Assets/__Scripts/UI/RhythmGame/BgmProgressBar.cs
@@ -4,23 +4,24 @@ using UnityEngine.UI;
 
 public class BgmProgressBar : MonoBehaviour
 {
-    [SerializeField] private AudioSource bgmPlayer;
-    [SerializeField] private TimeManager timeManager;
+    
     [SerializeField] private Image image;
     [SerializeField] private Material material;
+    [SerializeField] private TimeManager timeManager;
+    [SerializeField] private ChartManager chartManager;
     private Material instance;
     private static readonly int ProgressPropertyID = Shader.PropertyToID("_Progress");
 
     private void Awake()
     {
-        instance = new Material(material);
+        instance = new(material);
         image.material = instance;
     }
 
     private void Update()
     {
-        if (bgmPlayer.clip == null) return;
-        float length = bgmPlayer.clip.length * 1000;
+        if (chartManager.Chart.EndOfChart == null || chartManager.Chart == null) return;
+        float length = chartManager.Chart.EndOfChart.TimeMs;
         float time = timeManager.VisualTimeMs;
 
         float progress = Mathf.InverseLerp(0, length, time);
