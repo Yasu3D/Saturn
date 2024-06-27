9a8bb29abfc506957dc0e4fc63079d1b08a2083d
Yasu3D <l.frisan31@gmail.com>
2024-06-27 17:23:03 +0200

    use material instance instead of original material

--- Assets/__Scripts/RhythmGame/_Managers/BackgroundManager.cs
+++ Assets/__Scripts/RhythmGame/_Managers/BackgroundManager.cs
@@ -9,33 +9,41 @@ public class BackgroundManager : MonoBehaviour
 {
     [SerializeField] private ChartManager chartManager;
     [SerializeField] private TimeManager timeManager;
-    [SerializeField] private Material backgroundOrbParticleMaterial;
+    
+    [SerializeField] private ParticleSystemRenderer backgroundOrbParticleSystem;
+    private Material materialInstance;
     
     private static readonly int ParticleBpmProperty = Shader.PropertyToID("_BPM");
     private static readonly int ParticleVisualTimeProperty = Shader.PropertyToID("_VisualTime");
         
     private Chart Chart => chartManager.Chart;
     private int bgmDataIndex;
+
+    private void Awake()
+    {
+        materialInstance = new(backgroundOrbParticleSystem.material);
+        backgroundOrbParticleSystem.material = materialInstance;
+    }
+    
+    private void Update()
+    {
+        UpdateOrbParticles();
+    }
     
     private void UpdateOrbParticles()
     {
-        if (backgroundOrbParticleMaterial.HasFloat(ParticleVisualTimeProperty))
-            backgroundOrbParticleMaterial.SetFloat(ParticleVisualTimeProperty, timeManager.VisualTimeMs);
+        if (materialInstance.HasFloat(ParticleVisualTimeProperty))
+            materialInstance.SetFloat(ParticleVisualTimeProperty, timeManager.VisualTimeMs);
         
         if (bgmDataIndex > Chart.Notes.Count - 1) return;
 
         while (bgmDataIndex < Chart.BGMDataGimmicks.Count && Chart.BGMDataGimmicks[bgmDataIndex].TimeMs <= timeManager.VisualTimeMs)
         {
-            if (backgroundOrbParticleMaterial.HasFloat(ParticleBpmProperty))
-                backgroundOrbParticleMaterial.SetFloat(ParticleBpmProperty, Chart.BGMDataGimmicks[bgmDataIndex].BeatsPerMinute);
+            if (materialInstance.HasFloat(ParticleBpmProperty))
+                materialInstance.SetFloat(ParticleBpmProperty, Chart.BGMDataGimmicks[bgmDataIndex].BeatsPerMinute);
             
             bgmDataIndex++;
         }
-    }
-
-    private void Update()
-    {
-        UpdateOrbParticles();
     }
 }
 }
